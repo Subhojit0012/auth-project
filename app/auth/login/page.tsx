524d8c45a4b8eea4ff0a67e92ed1bc9b53762481
Subhojit0012 <subhojitroy00012@gmail.com>
2024-09-23 18:46:27 +0530

    mention the use client above the login form page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,4 @@
 import LoginForm from "@/components/auth/login-form";
-import { CardWrapper } from "@/components/auth/card-wrapper";
-
 const LoginPage = () => {
   return (
     <LoginForm />
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,26 +1,28 @@
-import * as z from 'zod'
-import {useForm} from 'react-hook-form'
-import {zodResolver} from '@hookform/resolvers/zod'
-import { LoginSchema } from '@/schemas'
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schemas";
 import {
   Form,
   FormControl,
   FormField,
   FormLabel,
   FormMessage,
-  FormItem
-} from "@/components/ui/form"
+  FormItem,
+} from "@/components/ui/form";
 
 import { CardWrapper } from "./card-wrapper";
 
 const LoginForm = () => {
   const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver :zodResolver(LoginSchema),
+    resolver: zodResolver(LoginSchema),
     defaultValues: {
       email: "",
-      password: ""
-    }
-  })
+      password: "",
+    },
+  });
 
   return (
     <CardWrapper

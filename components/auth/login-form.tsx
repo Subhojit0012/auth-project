7935b48743250fffe836188c615adaf72410779c
Subhojit0012 <subhojitroy00012@gmail.com>
2024-09-24 20:17:01 +0530

    email and password fields are done

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -14,6 +14,8 @@ import {
 } from "@/components/ui/form";
 
 import { CardWrapper } from "./card-wrapper";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 const LoginForm = () => {
   const form = useForm<z.infer<typeof LoginSchema>>({
@@ -31,7 +33,41 @@ const LoginForm = () => {
       backuttonHref="/auth/register"
       showSocial
     >
-      Login Form
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-4">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Emali</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="email" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,8 @@
 import * as z from 'zod'
 
 export const LoginSchema = z.object({
-    email: z.string().email(),
-    password: z.string()
+    email: z.string().email({
+        message: "Email required"
+    }),
+    password: z.string().max(10, {message: "Password is required"}).min(6, {message: "minimum six characters are required"})
 })

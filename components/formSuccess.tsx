1a4b6829de87505520e980606378d05b97b21276
Subhojit0012 <subhojitroy00012@gmail.com>
2024-09-25 18:00:24 +0530

    check comment in the file

--- components/formSuccess.tsx
+++ components/formSuccess.tsx
@@ -7,8 +7,9 @@ interface FromIconsProps{
 export const FormSuccess = ({messages}: FromIconsProps)=>{
     if(!messages) return null
 
+    // change the color of the text and the background
     return(
-        <div className='bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive'>
+        <div className='bg-emerald-500/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-emerald-500'>
             <CheckCircledIcon  className='h-4 w-4'/>
             <p>{messages}</p>
         </div>

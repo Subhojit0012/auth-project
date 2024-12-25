a34dbef4d8dab386f5ab1733509076cdcaa0e99a
Subhojit0012 <subhojitroy00012@gmail.com>
2024-12-25 13:36:24 +0530

    implement user registration with email verification and password hashing

--- actions/login.ts
+++ actions/login.ts
@@ -3,9 +3,9 @@
 import * as z from 'zod'
 import { LoginSchema } from '@/schemas'
 
-// get data from clients
+
 export const login = async (values: z.infer<typeof LoginSchema>)=> {
-    // vaildate fields to check errors or success
+    
     const validatefields = LoginSchema.safeParse(values)
 
     if(!validatefields.success){
--- actions/register.ts
+++ actions/register.ts
@@ -2,10 +2,13 @@
 
 import * as z from 'zod'
 import { RegisterSchema } from '@/schemas'
+import bcrypt from 'bcryptjs'
+import { db } from '@/lib/db'
+import { getUserByEmail } from '@/data/user'
 
 // get data from clients
 export const register = async (values: z.infer<typeof RegisterSchema>)=> {
-    // vaildate fields to check errors or success
+    //* vaildate fields to check errors or success
     const validatefields = RegisterSchema.safeParse(values)
 
     if(!validatefields.success){
@@ -13,6 +16,27 @@ export const register = async (values: z.infer<typeof RegisterSchema>)=> {
             error: "Invalid fields"
         }
     }
+
+    const {email, password, name} = validatefields.data
+    const hash = await bcrypt.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email)
+
+    if(existingUser){
+        return{
+            error: "User already exists"
+        }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hash
+        }
+    })
+
+    // TODO: send verification email
 
     return{
         success: "Email sent!"
